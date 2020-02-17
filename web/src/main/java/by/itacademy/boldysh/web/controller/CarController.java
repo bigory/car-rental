d59141e67b813718ba7777f327180c55df6ea7dd
bigory <igorboldysh@gmail.com>
2020-02-17 17:58:59 +0300

    First commit Fix test CarRepository and update web

--- web/src/main/java/by/itacademy/boldysh/web/controller/CarController.java
+++ web/src/main/java/by/itacademy/boldysh/web/controller/CarController.java
@@ -1,12 +1,12 @@
 package by.itacademy.boldysh.web.controller;
 
-import by.itacademy.boldysh.database.entity.Car;
-import by.itacademy.boldysh.database.entity.Transmission;
+import by.itacademy.boldysh.database.entity.*;
 import by.itacademy.boldysh.database.repository.CarRepository;
 import by.itacademy.boldysh.service.interfaces.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +20,11 @@ public class CarController {
     @ModelAttribute("transmissions")
     public Transmission[] getTransmissions() {
         return Transmission.values();
+    }
+
+    @ModelAttribute("carClass")
+    public CarClass[] getCarClass() {
+        return CarClass.values();
     }
 
     @Autowired
