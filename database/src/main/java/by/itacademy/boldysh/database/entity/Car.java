f97b0c7e797118425de0d564775520cc2bb744aa
bigory <igorboldysh@gmail.com>
2020-02-20 17:43:09 +0300

    Update method save

--- database/src/main/java/by/itacademy/boldysh/database/entity/Car.java
+++ database/src/main/java/by/itacademy/boldysh/database/entity/Car.java
@@ -10,15 +10,7 @@ import org.hibernate.annotations.OptimisticLockType;
 import org.hibernate.annotations.OptimisticLocking;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,7 +46,7 @@ public class Car extends BaseEntity<Long> {
     @Column(name = "cost_rental_of_day")
     private Integer costRentalOfDay;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "brand_car_id")
     private BrandCar brandCar;
 
