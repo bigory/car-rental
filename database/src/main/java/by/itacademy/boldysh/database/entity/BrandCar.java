e03210845e668e0950c3f12ecc800b1431222d22
bigory <igorboldysh@gmail.com>
2020-02-21 18:33:23 +0300

    Fix methodDelete

--- database/src/main/java/by/itacademy/boldysh/database/entity/BrandCar.java
+++ database/src/main/java/by/itacademy/boldysh/database/entity/BrandCar.java
@@ -26,7 +26,7 @@ public class BrandCar extends BaseEntity<Long> {
     @Column(name = "brand")
     private String brand;
 
-    @OneToMany(mappedBy = "brandCar", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "brandCar",  fetch = FetchType.EAGER)
     private List<Car> cars = new ArrayList<>();
 
     public BrandCar(String brand) {
