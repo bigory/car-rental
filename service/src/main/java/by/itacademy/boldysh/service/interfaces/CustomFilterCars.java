fe2b20fc6b8170c9736ffc745f7b06ec74c17b04
bigory <igorboldysh@gmail.com>
2020-03-17 18:08:15 +0300

    Create FilterCars

---
+++ service/src/main/java/by/itacademy/boldysh/service/interfaces/CustomFilterCars.java
@@ -0,0 +1,13 @@
+package by.itacademy.boldysh.service.interfaces;
+
+import by.itacademy.boldysh.database.entity.BrandCar;
+import by.itacademy.boldysh.database.entity.Car;
+import by.itacademy.boldysh.database.entity.CarClass;
+import by.itacademy.boldysh.database.entity.Transmission;
+
+import java.util.List;
+
+public interface CustomFilterCars {
+
+    List<Car> findByFilterCars(String model, Integer yearOfIssue, Transmission transmission, CarClass carClass, Integer costRentalOfDay, BrandCar brandCar);
+}
