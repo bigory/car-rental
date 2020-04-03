dd845da9ba6d2ccd9b36124b98b234f3540485d6
bigory <igorboldysh@gmail.com>
2020-04-03 17:25:45 +0300

    add security

---
+++ web/src/main/java/by/itacademy/boldysh/web/initializer/SecurityInitializer.java
@@ -0,0 +1,8 @@
+/*
+package by.itacademy.boldysh.web.initializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
+*/
