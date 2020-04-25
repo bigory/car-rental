b9dae8a773730066c35b6b5424116da7fd80548b
bigory <igorboldysh@gmail.com>
2020-04-25 14:24:45 +0300

    update BlackListUserServiceService,add page black-list-user.html,create BlackListController.Update delete-car.html,car.html

--- service/src/main/java/by/itacademy/boldysh/service/impl/BlackListUserServiceServiceImpl.java
+++ service/src/main/java/by/itacademy/boldysh/service/impl/BlackListUserServiceServiceImpl.java
@@ -1,14 +1,18 @@
 package by.itacademy.boldysh.service.impl;
 
 import by.itacademy.boldysh.database.entity.BlackListUserService;
+import by.itacademy.boldysh.database.entity.Car;
 import by.itacademy.boldysh.database.repository.BlackListUserServiceRepository;
 import by.itacademy.boldysh.service.interfaces.BlackListUserServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -40,7 +44,21 @@ public class BlackListUserServiceServiceImpl implements BlackListUserServiceServ
     }
 
     @Override
-    public Page<BlackListUserService> findByPaginated(Pageable pageable, List<BlackListUserService> entities) {
-        return null;
+    public Page<BlackListUserService> findByPaginated(Pageable pageable, List<BlackListUserService> blackListUserServices) {
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<BlackListUserService> listUserServices;
+
+        if (blackListUserServices.size() < startItem) {
+            listUserServices = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, blackListUserServices.size());
+            listUserServices = blackListUserServices.subList(startItem, toIndex);
+        }
+
+        Page<BlackListUserService> blackListUserServicePage = new PageImpl<BlackListUserService>(listUserServices, PageRequest.of(currentPage, pageSize), blackListUserServices.size());
+
+        return blackListUserServicePage;
     }
 }
